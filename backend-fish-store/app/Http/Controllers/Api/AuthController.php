94912d3f2463a98ed51194582d3be0cc3dedb192
eevan7a9 <e7a9@protonmail.com>
2019-11-06 08:45:40 +0900

    added user authentication

---
+++ backend-fish-store/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|min:4|max:55',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed',
+            ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        } else {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            $access_token = $user->createToken('fishstore')->accessToken;
+            $user->makeHidden('id');
+            return response()->json(['user' => $user, 'access_token' => $access_token], 200);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $access_token = $user->createToken('AppName')->accessToken;
+
+            return response()->json(['user' => $user, 'access_token' => $access_token], 200);
+        } else {
+            return response()->json(['message' => 'Invalid Credentials'], 401);
+        }
+    }
+
+    public function userInfo()
+    {
+        $user = Auth::user();
+        return response()->json(['user' => $user]);
+    }
+
+    public function logout()
+    {
+        // we get the authenticated user
+        $user = Auth::user()->token();
+        // we set revoked to 1
+        $user->revoke();
+        return response()->json(["message" => "successfully loged out!!!"]);
+
+        /** if you want to logout the user from all devices
+         *
+         * DB::table('oauth_access_tokens')
+         *   ->where('user_id', Auth::user()->id)
+         *   ->update([
+         *       'revoked' => true
+         *     ]);
+         */
+    }
+}
--- backend-fish-store/routes/api.php
+++ backend-fish-store/routes/api.php
@@ -11,8 +11,19 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () { // domain/api/v1/
+
+    Route::post('/register', 'Api\AuthController@register');
+    Route::post('/login', 'Api\AuthController@login');
+
+    Route::group(['middleware' => ['auth:api']], function () { // Authenticated Users Only
+        Route::get('/user', 'Api\AuthController@userInfo');
+        Route::get('/logout', 'Api\AuthController@logout');
+    });
 });
