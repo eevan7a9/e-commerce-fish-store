8165ffdb084092e2777612388b6edb188009c8f9
eevan7a9 <e7a9@protonmail.com>
2019-11-06 04:50:09 +0900

    added header style

--- frontend-fish-store/src/components/FeaturedProducts.vue
+++ frontend-fish-store/src/components/FeaturedProducts.vue
@@ -1,49 +1,56 @@
 <template>
-  <div class="row mt-5">
-    <div class="col-md-4">
-      <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="../assets/img/f1.jpg" alt="Card image cap" />
-        <div class="card-body">
-          <p
-            class="card-text"
-          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+  <div>
+    <section class="row align-items-center m-5">
+      <div class="h-400 col-12 text-center text-dark">
+        <h2 class="font-weight-light featured-header text-inner-shadow-success">Featured Products</h2>
+      </div>
+    </section>
+    <div class="row mt-5">
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" src="../assets/img/f1.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+              </div>
+              <small class="text-muted">9 mins</small>
             </div>
-            <small class="text-muted">9 mins</small>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="../assets/img/f2.jpg" alt="Card image cap" />
-        <div class="card-body">
-          <p
-            class="card-text"
-          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" src="../assets/img/f2.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+              </div>
+              <small class="text-muted">9 mins</small>
             </div>
-            <small class="text-muted">9 mins</small>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="../assets/img/f3.jpg" alt="Card image cap" />
-        <div class="card-body">
-          <p
-            class="card-text"
-          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" src="../assets/img/f3.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+              </div>
+              <small class="text-muted">9 mins</small>
             </div>
-            <small class="text-muted">9 mins</small>
           </div>
         </div>
       </div>
@@ -58,8 +65,12 @@ export default {
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css?family=Staatliches&display=swap");
 img {
   width: 100%;
   height: 300px;
+}
+.featured-header {
+  font-family: "Staatliches", cursive;
 }
 </style>
