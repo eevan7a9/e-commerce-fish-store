72a6947831c02b029551bed2ee533f5ddf2c393a
eevan7a9 <e7a9@protonmail.com>
2019-11-05 14:02:14 +0900

    added showcase image and featured article

---
+++ frontend-fish-store/src/components/FeaturedArticle.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <!-- START THE FEATURETTES -->
+    <hr class="featurette-divider" />
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">
+          First featurette heading.
+          <span class="text-muted">It'll blow your mind.</span>
+        </h2>
+        <p
+          class="lead"
+        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <img
+          class="featurette-image img-fluid mx-auto"
+          data-src="holder.js/500x500/auto"
+          alt="Generic placeholder image"
+          src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg"
+        />
+      </div>
+    </div>
+
+    <hr class="featurette-divider" />
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">
+          Oh yeah, it's that good.
+          <span class="text-muted">See for yourself.</span>
+        </h2>
+        <p
+          class="lead"
+        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img
+          class="featurette-image img-fluid mx-auto"
+          data-src="holder.js/500x500/auto"
+          alt="Generic placeholder image"
+          src="https://cdn.pixabay.com/photo/2019/08/16/10/27/koi-fish-4410009_960_720.jpg"
+        />
+      </div>
+    </div>
+
+    <hr class="featurette-divider" />
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">
+          And lastly, this one.
+          <span class="text-muted">Checkmate.</span>
+        </h2>
+        <p
+          class="lead"
+        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <img
+          class="featurette-image img-fluid mx-auto"
+          data-src="holder.js/500x500/auto"
+          src="https://cdn.pixabay.com/photo/2019/04/02/19/29/madeira-4098596_960_720.jpg"
+          alt="Generic placeholder image"
+        />
+      </div>
+    </div>
+
+    <hr class="featurette-divider" />
+
+    <!-- /END THE FEATURETTES -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeaturedArticle"
+};
+</script>
+
+<style>
+</style>
---
+++ frontend-fish-store/src/components/ShowcaseImage.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="masterhead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-center showcase-header-bg text-white">
+          <h1 class="font-weight-light">Organic Fed Fish</h1>
+          <p class="lead">We grow the kois the way a koi normally should.</p>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "ShowcaseImage"
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Anton&display=swap");
+.masterhead {
+  height: 60vh;
+  min-height: 500px;
+  background-image: url("https://cdn.pixabay.com/photo/2014/04/21/11/51/koi-329076_960_720.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.showcase-header-bg {
+  background: #0000003b;
+  padding: 10px 15px;
+}
+.masterhead h1 {
+  font-family: "Anton", sans-serif;
+  letter-spacing: 2px;
+  text-shadow: 3px 3px 5px #050505, -1px -1px 5px #413f3f, 1px -1px 5px #413f3f,
+    -1px 1px 5px #413f3f, 1px 1px 5px #413f3f;
+}
+</style>
