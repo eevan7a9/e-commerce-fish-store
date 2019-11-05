b7cf7bd61f40a58bc1e91faae24619d33f0a8373
eevan7a9 <e7a9@protonmail.com>
2019-11-05 14:01:44 +0900

    added featured products

---
+++ frontend-fish-store/src/components/FeaturedProducts.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="row mt-5">
+    <div class="col-md-4">
+      <div class="card mb-4 box-shadow">
+        <img class="card-img-top" src="../assets/img/f1.jpg" alt="Card image cap" />
+        <div class="card-body">
+          <p
+            class="card-text"
+          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+            </div>
+            <small class="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card mb-4 box-shadow">
+        <img class="card-img-top" src="../assets/img/f2.jpg" alt="Card image cap" />
+        <div class="card-body">
+          <p
+            class="card-text"
+          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+            </div>
+            <small class="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card mb-4 box-shadow">
+        <img class="card-img-top" src="../assets/img/f3.jpg" alt="Card image cap" />
+        <div class="card-body">
+          <p
+            class="card-text"
+          >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-success p-3">$30 | A piece</button>
+            </div>
+            <small class="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeaturedProducts"
+};
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 300px;
+}
+</style>
