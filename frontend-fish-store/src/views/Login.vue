ec9207ad59925e2d45b044e0d716f44d3c01fbae
eevan7a9 <e7a9@protonmail.com>
2019-11-07 03:19:55 +0900

    added login page

---
+++ frontend-fish-store/src/views/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="login-form">
+    <form action @submit.prevent="submit">
+      <h2 class="text-center">Log in</h2>
+      <div class="form-group">
+        <input
+          type="email"
+          class="form-control"
+          v-model="email"
+          placeholder="Email"
+          required="required"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          v-model="password"
+          placeholder="Password"
+          required="required"
+        />
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-success btn-block">Log in</button>
+      </div>
+      <!-- <div class="clearfix">
+        <label class="pull-left checkbox-inline">
+          <input type="checkbox" /> Remember me
+        </label>
+        <a href="#" class="pull-right">Forgot Password?</a>
+      </div>-->
+    </form>
+    <p class="text-center">
+      <a href="#">Create an Account</a>
+    </p>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+// import FeaturedArticle from "../components/FeaturedArticle";
+// import { mapGetters } from "vuex";
+export default {
+  name: "login",
+  data() {
+    return {
+      user: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      alert(`${this.email} ${this.password}`);
+    }
+  }
+};
+</script>
+<style scoped>
+.login-form {
+  width: 500px;
+  margin: 50px auto;
+}
+.login-form form {
+  margin-bottom: 15px;
+  background: #f7f7f7;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  padding: 30px;
+}
+.login-form h2 {
+  margin: 0 0 15px;
+}
+.form-control,
+.btn {
+  min-height: 38px;
+  border-radius: 2px;
+}
+.btn {
+  font-size: 15px;
+  font-weight: bold;
+}
+</style>
