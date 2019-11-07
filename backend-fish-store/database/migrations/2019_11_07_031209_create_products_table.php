ac2cd7872297f9a4caa6754080669ed2f5e42143
eevan7a9 <e7a9@protonmail.com>
2019-11-07 12:53:09 +0900

    added products

---
+++ backend-fish-store/database/migrations/2019_11_07_031209_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 9, 2);
+            $table->decimal('weight', 5, 2);
+            $table->integer('units')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
