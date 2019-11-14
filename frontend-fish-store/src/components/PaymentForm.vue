e8f87630700e8af120cace40491eea352c04de93
eevan7a9 <e7a9@protonmail.com>
2019-11-15 01:08:27 +0900

    adding billing with stripe elements

--- frontend-fish-store/src/components/PaymentForm.vue
+++ frontend-fish-store/src/components/PaymentForm.vue
@@ -1,27 +1,11 @@
 <template>
-    <div ref="card">
-        <button @click="purchase">Purchase</button>
-        <h1>Hello</h1>
-    </div>
+
 </template>
 <script>
-    let stripe = Stripe(`YOUR_STRIPE_PUBLISHABLE_KEY`),
-        elements = stripe.elements(),
-        card = undefined;
     export default {
         name: "PaymentForm",
-        mounted: function() {
-            card = elements.create('card');
-            card.mount(this.$refs.card);
-        },
-        purchase: function() {
-            stripe.createToken(card).then(function(result) {
-                if (result.error) {
-                    self.hasCardErrors = true;
-                    self.$forceUpdate(); // Forcing the DOM to update so the Stripe Element can update.
-                    return;
-                }
-            });
+        methods: {
+
         }
     };
-</script>h
+</script>
