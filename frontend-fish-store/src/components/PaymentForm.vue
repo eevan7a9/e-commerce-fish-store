5f1d114af8f9b9d11436c66d3493a1481ef7b688
eevan7a9 <e7a9@protonmail.com>
2019-11-15 04:54:31 +0900

    added additional info for stripe element

--- frontend-fish-store/src/components/PaymentForm.vue
+++ frontend-fish-store/src/components/PaymentForm.vue
@@ -3,29 +3,42 @@
         <hr>
         <h4>Billing Details :</h4>
         <div class="form-row">
-            <div class="form-group col-sm-5">
-                <label>Name</label>
+            <div class="form-group col-sm-6">
+                <label class="text-capitalize">Name</label>
                 <input type="text" v-model="billing.name" class="form-control">
             </div>
-            <div class="form-group col-sm-5">
-                <label>Email</label>
-                <input type="text" v-model="billing.email" class="form-control">
+            <div class="form-group col-sm-6">
+                <label class="text-capitalize">Email</label>
+                <input type="email" v-model="billing.email" class="form-control">
             </div>
-            <div class="form-group col-sm-5">
-                <label>Address</label>
+            <div class="form-group col-sm-6">
+                <label class="text-capitalize">Address</label>
                 <input type="text" v-model="billing.address" class="form-control">
             </div>
-            <div class="form-group col-sm-5">
-                <label>Phone</label>
+            <div class="form-group col-sm-6">
+                <label class="text-capitalize">Phone</label>
                 <input type="text" v-model="billing.phone" class="form-control">
             </div>
-            <div class="form-group col-sm-5">
-                <label>City</label>
+            <div class="form-group col-sm-4">
+                <label class="text-capitalize">City</label>
                 <input type="text" v-model="billing.city" class="form-control">
             </div>
-            <div class="form-group col-sm-5">
-                <label>Province</label>
+            <div class="form-group col-sm-4">
+                <label class="text-capitalize">Province</label>
                 <input type="text" v-model="billing.province" class="form-control">
+            </div>
+            <div class="form-group col-sm-4">
+                <label for="sel1">Select list (select one):</label>
+                <select class="form-control" id="sel1" v-model="billing.country">
+                    <option value="us">US</option>
+                    <option value="jp">JP</option>
+                    <option value="ph">PH</option>
+                    <option value="au">AU</option>
+                </select>
+            </div>
+            <div class="form-group col-sm-4">
+                <label class="text-capitalize">postal code</label>
+                <input type="number" v-model="billing.postal_code" class="form-control">
             </div>
         </div>
         <hr>
@@ -48,7 +61,9 @@
                     address: "",
                     phone: "0",
                     city: "",
-                    province: ""
+                    province: "",
+                    country: "",
+                    postal_code: 0,
                 },
                 card: null,
                 elements: null
@@ -62,7 +77,16 @@
         methods: {
             purchase: function() {
                 let self = this;
-                stripe.createToken(card).then(function(result) {
+                const additional_data = {
+                    name: this.billing.name,
+                    address_line1: this.billing.address,
+                    address_city: this.billing.city,
+                    address_state: this.billing.province,
+                    address_zip: this.billing.zip,
+                    address_country: this.billing.country,
+                    currency: "usd",
+                }
+                stripe.createToken(card, additional_data).then(function(result) {
                     if (result.error) {
                         self.hasCardErrors = true;
                         self.$forceUpdate(); // Forcing the DOM to update so the Stripe Element can update.
