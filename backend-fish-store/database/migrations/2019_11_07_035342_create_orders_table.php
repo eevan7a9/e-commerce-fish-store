d8c6a45514e3043a3c9249d654f75f341ee9ca3d
eevan7a9 <e7a9@protonmail.com>
2019-11-17 21:57:51 +0900

    change data attributes

--- backend-fish-store/database/migrations/2019_11_07_035342_create_orders_table.php
+++ backend-fish-store/database/migrations/2019_11_07_035342_create_orders_table.php
@@ -15,18 +15,19 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('description')->nullable();
             $table->unsignedInteger('quantity')->default(1);
-            $table->decimal('total_price', 9, 2);
-            $table->decimal('total_weight', 5, 2);
+            $table->decimal('amount', 9, 2);
+            $table->decimal('weight', 5, 2);
             $table->string('address');
             $table->string('country');
             $table->string('city');
             $table->string('state');
             $table->integer('postal_code')->nullable();
             $table->string('phone');
+            $table->string('email');
             $table->string('charge_id')->nullable();
             $table->integer('last4')->nullable();
             $table->string('receipt_url')->nullable();
