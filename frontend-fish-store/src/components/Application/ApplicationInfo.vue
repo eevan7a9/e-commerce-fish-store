72eb96b6112024a6c8efd4dffde7ecda89cbd164
eevan7a9 <e7a9@protonmail.com>
2019-11-19 22:23:19 +0900

    added new icons and modifiy sidebar

--- frontend-fish-store/src/components/Application/ApplicationInfo.vue
+++ frontend-fish-store/src/components/Application/ApplicationInfo.vue
@@ -1,92 +1,63 @@
 <template>
-  <div>
     <div>
-      <b-card-group deck>
-        <b-card bg-variant="primary" text-variant="white" header="Products" class="text-center">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-package"
-            >
-              <line x1="16.5" y1="9.4" x2="7.5" y2="4.21" />
-              <path
-                d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"
-              />
-              <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
-              <line x1="12" y1="22.08" x2="12" y2="12" />
-            </svg>
-          </span>
-          <hr />
-          <h3 class="mt-3 font-weight-bold">23</h3>
-        </b-card>
-
-        <b-card bg-variant="secondary" text-variant="white" header="Users" class="text-center">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-users"
-            >
-              <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
-              <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
-          </span>
-          <hr />
-          <h3 class="mt-3 font-weight-bold">7</h3>
-        </b-card>
-
-        <b-card bg-variant="danger" text-variant="white" header="Order" class="text-center">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-clipboard"
-            >
-              <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
-              <rect x="8" y="2" width="8" height="4" rx="1" ry="1" />
-            </svg>
-          </span>
-          <hr />
-          <h3 class="mt-3 font-weight-bold">13</h3>
-        </b-card>
-      </b-card-group>
+        <div>
+            <b-card-group deck>
+                <b-card bg-variant="primary" text-variant="white" header="Products" class="text-center">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package">
+                            <line x1="16.5" y1="9.4" x2="7.5" y2="4.21" />
+                            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
+                            <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
+                            <line x1="12" y1="22.08" x2="12" y2="12" />
+                        </svg>
+                    </span>
+                    <hr />
+                    <h3 class="mt-3 font-weight-bold">23</h3>
+                </b-card>
+                <b-card bg-variant="secondary" text-variant="white" header="Registered" class="text-center">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
+                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+                            <circle cx="9" cy="7" r="4" />
+                            <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
+                            <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                        </svg>
+                    </span>
+                    <hr />
+                    <h3 class="mt-3 font-weight-bold">7</h3>
+                </b-card>
+                <b-card bg-variant="danger" text-variant="white" header="Pending Order" class="text-center">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard">
+                            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+                            <rect x="8" y="2" width="8" height="4" rx="1" ry="1" />
+                        </svg>
+                    </span>
+                    <hr />
+                    <h3 class="mt-3 font-weight-bold">13</h3>
+                </b-card>
+                <b-card bg-variant="success" text-variant="white" header="Sale Made" class="text-center">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <line x1="12" y1="1" x2="12" y2="23" />
+                            <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                        </svg>
+                    </span>
+                    <hr />
+                    <h3 class="mt-3 font-weight-bold">55</h3>
+                </b-card>
+            </b-card-group>
+        </div>
     </div>
-  </div>
 </template>
-
 <script>
-export default {
-  name: "ApplicationInfo"
-};
+    export default {
+        name: "ApplicationInfo"
+    };
 </script>
-
 <style scoped>
-svg {
-  width: 100px;
-  height: 100px;
-}
+    svg {
+        width: 50px;
+        height: 50px;
+    }
 </style>
--- frontend-fish-store/src/views/Application.vue
+++ frontend-fish-store/src/views/Application.vue
@@ -1,88 +1,96 @@
 <template>
-  <div class="container wrapper pt-5">
-    <div class="sidebar pr-2">
-      <b-list-group>
-        <!-- Application Section Starts -->
-        <b-list-group-item
-          v-b-toggle.collapse-3
-          class="pointer font-weight-bolder text-light bg-success"
-        >Application</b-list-group-item>
-        <b-collapse visible id="collapse-3" class="pl-3 bg-success">
-          <b-list-group>
-            <b-list-group-item class="bg-light">
-              <router-link :to="{name:'application.info'}" class="text-secondary">App Status</router-link>
-            </b-list-group-item>
-            <b-list-group-item class="bg-light text-secondary">Layout</b-list-group-item>
-          </b-list-group>
-        </b-collapse>
-        <!-- Application Ends -->
-        <!-- Products Section Starts-->
-        <b-list-group-item
-          v-b-toggle.collapse-1
-          class="pointer font-weight-bolder text-light bg-success"
-        >Products</b-list-group-item>
-        <b-collapse visible id="collapse-1" class="pl-3 bg-success">
-          <b-list-group>
-            <b-list-group-item class="bg-light">
-              <router-link :to="{name:'application.products'}" class="text-secondary">Data Table</router-link>
-            </b-list-group-item>
-
-            <b-list-group-item class="bg-light">
-              <router-link
-                :to="{name:'application.products.add'}"
-                class="text-secondary"
-              >Add Products</router-link>
-            </b-list-group-item>
-          </b-list-group>
-        </b-collapse>
-        <!-- Products End -->
-        <!-- Orders Section Starts -->
-        <b-list-group-item
-          v-b-toggle.collapse-2
-          class="pointer font-weight-bolder text-light bg-success"
-        >Orders</b-list-group-item>
-        <b-collapse visible id="collapse-2" class="pl-3 bg-success">
-          <b-list-group>
-            <b-list-group-item class="bg-light">
-              <router-link :to="{name:'application.orders'}" class="text-secondary">Data Table</router-link>
-            </b-list-group-item>
-            <b-list-group-item class="bg-light text-secondary">Add Order</b-list-group-item>
-          </b-list-group>
-        </b-collapse>
-        <!-- Orders End -->
-      </b-list-group>
+    <div class=" wrapper">
+        <div class="sidebar bg-dark">
+            <b-list-group>
+                <!-- Application Section Starts -->
+                <b-list-group-item v-b-toggle.collapse-3 class="pointer font-weight-bolder text-light bg-dark">Application
+                    <img src="../assets/img/chevron-down.svg" alt="">
+                </b-list-group-item>
+                <b-collapse visible id="collapse-3" class="pl-3 bg-dark">
+                    <b-list-group>
+                        <b-list-group-item class="bg-secondary">
+                            <router-link :to="{name:'application.info'}" class="text-light">App Status
+    <img src="../assets/img/bar-chart-2.svg" class="ml-1" alt="">
+                            </router-link>
+                        </b-list-group-item>
+                        <!-- <b-list-group-item class="bg-secondary text-light">Layout</b-list-group-item> -->
+                    </b-list-group>
+                </b-collapse>
+                <!-- Application Ends -->
+                <!-- Products Section Starts-->
+                <b-list-group-item v-b-toggle.collapse-1 class="pointer font-weight-bolder text-light bg-dark">Products
+                    <img src="../assets/img/chevron-down.svg" alt="">
+                </b-list-group-item>
+                <b-collapse visible id="collapse-1" class="pl-3 bg-dark">
+                    <b-list-group>
+                        <b-list-group-item class="bg-secondary">
+                            <router-link :to="{name:'application.products'}" class="text-light">Data Table
+                                <img src="../assets/img/file-text.svg" class="ml-1" alt="">
+                            </router-link>
+                        </b-list-group-item>
+                        <b-list-group-item class="bg-secondary">
+                            <router-link :to="{name:'application.products.add'}" class="text-light">Add Products
+                                <img src="../assets/img/plus-square.svg" class="ml-1" alt=""></router-link>
+                        </b-list-group-item>
+                    </b-list-group>
+                </b-collapse>
+                <!-- Products End -->
+                <!-- Orders Section Starts -->
+                <b-list-group-item v-b-toggle.collapse-2 class="pointer font-weight-bolder text-light bg-dark">Orders
+                    <img src="../assets/img/chevron-down.svg" alt="">
+                </b-list-group-item>
+                <b-collapse visible id="collapse-2" class="pl-3 bg-dark">
+                    <b-list-group>
+                        <b-list-group-item class="bg-secondary">
+                            <router-link :to="{name:'application.orders'}" class="text-light">Data Table
+                                <img src="../assets/img/file-text.svg" class="ml-1" alt="">
+                            </router-link>
+                        </b-list-group-item>
+                        <!-- <b-list-group-item class="bg-secondary text-light">Add Order</b-list-group-item> -->
+                    </b-list-group>
+                </b-collapse>
+                <!-- Orders End -->
+            </b-list-group>
+        </div>
+        <div class="main container  pt-5">
+            <transition name="fade" mode="out-in">
+                <router-view />
+            </transition>
+        </div>
     </div>
-    <div class="main">
-      <transition name="fade" mode="out-in">
-        <router-view />
-      </transition>
-    </div>
-  </div>
 </template>
-
 <script>
-export default {
-  name: "Application"
-};
+    export default {
+        name: "Application"
+    };
 </script>
-
 <style scoped>
-a:hover {
-  text-decoration: none;
-}
-.wrapper {
-  display: grid;
-  grid-template-columns: auto 1fr;
-}
-.sidebar {
-  /* background: rebeccapurple; */
-  width: 300px;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
+    a:hover {
+        text-decoration: none;
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-columns: auto 1fr;
+    }
+
+    .sidebar {
+        /* background: rebeccapurple; */
+        width: 300px;
+        background: red;
+        height: 100vh;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.3s;
+    }
+
+    .fade-enter,
+    .fade-leave-to
+
+    /* .fade-leave-active below version 2.1.8 */
+        {
+        opacity: 0;
+    }
 </style>
