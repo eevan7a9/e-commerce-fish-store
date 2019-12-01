ff82bbe0a20af0d847c302e9aa09796e856875ea
eevan7a9 <e7a9@protonmail.com>
2019-12-01 10:44:44 +0900

    added validation on-input and sample card

--- frontend-fish-store/src/views/CheckoutStripe.vue
+++ frontend-fish-store/src/views/CheckoutStripe.vue
@@ -17,32 +17,32 @@
                     <div class="form-row">
                         <div class="form-group col-sm-6">
                             <label class="text-capitalize">Name</label>
-                            <input type="text" v-model="billing.name" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.name" class="form-control" required />
                             <p class="text-danger" v-if="error.name">Required</p>
                         </div>
                         <div class="form-group col-sm-6">
                             <label class="text-capitalize">Email</label>
-                            <input type="email" v-model="billing.email" class="form-control" required />
+                            <input @input="validation" type="email" v-model="billing.email" class="form-control" required />
                             <p class="text-danger" v-if="error.email">Required</p>
                         </div>
                         <div class="form-group col-sm-6">
                             <label class="text-capitalize">Address</label>
-                            <input type="text" v-model="billing.address" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.address" class="form-control" required />
                             <p class="text-danger" v-if="error.address">Required</p>
                         </div>
                         <div class="form-group col-sm-6">
                             <label class="text-capitalize">Phone</label>
-                            <input type="text" v-model="billing.phone" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.phone" class="form-control" required />
                             <p class="text-danger" v-if="error.phone">Required</p>
                         </div>
                         <div class="form-group col-sm-4">
                             <label class="text-capitalize">City</label>
-                            <input type="text" v-model="billing.city" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.city" class="form-control" required />
                             <p class="text-danger" v-if="error.city">Required</p>
                         </div>
                         <div class="form-group col-sm-4">
                             <label class="text-capitalize">Province</label>
-                            <input type="text" v-model="billing.province" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.province" class="form-control" required />
                             <p class="text-danger" v-if="error.province">Required</p>
                         </div>
                         <div class="form-group col-sm-4">
@@ -57,7 +57,7 @@
                         </div>
                         <div class="form-group col-sm-4">
                             <label class="text-capitalize">postal code</label>
-                            <input type="text" v-model="billing.postal_code" class="form-control" required />
+                            <input @input="validation" type="text" v-model="billing.postal_code" class="form-control" required />
                             <p class="text-danger" v-if="error.postal_code">Required</p>
                         </div>
                     </div>
@@ -65,6 +65,13 @@
                     <h4>Payment Details :</h4>
                     <StripeCards class="mb-2" />
                     <div ref="card" class="p-3 mb-2 border rounded"></div>
+                    <small v-b-toggle.collapse-1 class="text-info font-weight-bold" style="cursor:pointer;">DEMO CARD: click me!</small>
+                    <b-collapse id="collapse-1" class="mt-2">
+                        <div class="bg-light text-secondary d-flex justify-content-between">
+                            <span>CARD: 4242 4242 4242 4242</span>
+                            <span>04 / 24 [any] [any]</span>
+                        </div>
+                    </b-collapse>
                     <button v-on:click="purchase" class="btn btn-success form-control font-weight-bold text-uppercase mt-3">Purchase</button>
                 </div>
             </div>
