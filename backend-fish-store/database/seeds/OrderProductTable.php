ece4c7020221e93166b4634d9875b8a675970a93
eevan7a9 <e7a9@protonmail.com>
2019-12-02 10:30:39 +0900

    updated seeders

--- backend-fish-store/database/seeds/OrderProductTable.php
+++ backend-fish-store/database/seeds/OrderProductTable.php
@@ -13,54 +13,92 @@ class OrderProductTable extends Seeder
     public function run()
     {
         DB::table('order_product')->insert([
-            "order_id" => 1,
-            "product_id" => 1,
-            "quantity" => 2,
-            "amount" => 466.80,
-            "weight" => 13.50
+            "id" => 8,
+            "order_id" => 5,
+            "product_id" => 5,
+            "quantity" => 4,
+            "amount" => 482.00,
+            "weight" => 12.60
         ]);
         DB::table('order_product')->insert([
-            "order_id" => 1,
-            "product_id" => 2,
-            "quantity" => 2,
-            "amount" => 466.80,
-            "weight" => 13.50
-        ]);
-        DB::table('order_product')->insert([
-            "order_id" => 1,
+            "id" => 9,
+            "order_id" => 5,
             "product_id" => 3,
             "quantity" => 1,
-            "amount" => 466.80,
-            "weight" => 13.50
+            "amount" => 220.50,
+            "weight" => 1.25
         ]);
         DB::table('order_product')->insert([
-            "order_id" => 2,
-            "product_id" => 3,
-            "quantity" => 10,
-            "amount" => 1400.30,
-            "weight" => 13.50
-        ]);
-        DB::table('order_product')->insert([
-            "order_id" => 3,
+            "id" => 10,
+            "order_id" => 5,
             "product_id" => 2,
-            "quantity" => 5,
-            "amount" => 700.25,
-            "weight" => 13.50
+            "quantity" => 1,
+            "amount" => 220.50,
+            "weight" => 2.25
         ]);
         DB::table('order_product')->insert([
-            "order_id" => 3,
-            "product_id" => 1,
-            "quantity" => 5,
-            "amount" => 700.25,
-            "weight" => 13.50
+            "id" => 13,
+            "order_id" => 7,
+            "product_id" => 3,
+            "quantity" => 8,
+            "amount" => 1764.00,
+            "weight" => 10.00
         ]);
-
         DB::table('order_product')->insert([
-            "order_id" => 4,
+            "id" => 14,
+            "order_id" => 7,
+            "product_id" => 2,
+            "quantity" => 11,
+            "amount" => 2425.50,
+            "weight" => 24.75
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 15,
+            "order_id" => 8,
+            "product_id" => 3,
+            "quantity" => 1,
+            "amount" => 220.50,
+            "weight" => 1.25
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 16,
+            "order_id" => 8,
+            "product_id" => 2,
+            "quantity" => 1,
+            "amount" => 220.50,
+            "weight" => 2.25
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 17,
+            "order_id" => 8,
             "product_id" => 5,
-            "quantity" => 10,
-            "amount" => 1400.50,
-            "weight" => 13.50
+            "quantity" => 1,
+            "amount" => 120.50,
+            "weight" => 3.15
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 18,
+            "order_id" => 8,
+            "product_id" => 4,
+            "quantity" => 1,
+            "amount" => 120.50,
+            "weight" => 2.25
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 20,
+            "order_id" => 10,
+            "product_id" => 5,
+            "quantity" => 1,
+            "amount" => 120.50,
+            "weight" => 3.15
+        ]);
+        DB::table('order_product')->insert([
+            "id" => 21,
+            "order_id" => 10,
+            "product_id" => 2,
+            "quantity" => 1,
+            "amount" => 220.50,
+            "weight" => 2.15
         ]);
 
     }
