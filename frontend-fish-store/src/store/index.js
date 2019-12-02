9699db3568aa14ab2bccbfe1f85ee551cf62b95f
eevan7a9 <e7a9@protonmail.com>
2019-12-02 21:40:22 +0900

    insert url

--- frontend-fish-store/src/store/index.js
+++ frontend-fish-store/src/store/index.js
@@ -9,7 +9,8 @@ import loader from "./modules/loader";
 
 Vue.use(Vuex)
 
-axios.defaults.baseURL = "http://localhost:8000/public/api/v1/";
+axios.defaults.baseURL = "http://onlinestorefish.rf.gd/public/api/v1/";
+// axios.defaults.baseURL = "http://localhost:8000/public/api/v1/";
 
 export default new Vuex.Store({
   modules: {
