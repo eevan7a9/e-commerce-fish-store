e50ae119bae04bed8eca205a32e547242e02872a
eevan7a9 <e7a9@protonmail.com>
2020-03-23 11:13:04 +0900

    added sweetAlert2

--- frontend-fish-store/src/main.js
+++ frontend-fish-store/src/main.js
@@ -2,14 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./router/router-guard"
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import "./router/router-guard"
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 Vue.use(BootstrapVue);
+Vue.use(VueSweetalert2);
+
 Vue.config.productionTip = false
 
 new Vue({
