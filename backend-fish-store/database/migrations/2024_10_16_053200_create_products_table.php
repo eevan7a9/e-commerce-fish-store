e2eda6ca769fe6acbfb85b075a420db3ce648bea
eevan7a9 <eevan7a9@gmail.com>
2024-10-17 10:10:53 +0800

    rebuild Products and added Category & tags

---
+++ backend-fish-store/database/migrations/2024_10_16_053200_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->decimal('weight', 5, 2)->nullable();
+            $table->integer('units')->default(1)->unsigned();
+            $table->text('description')->nullable();
+            $table->json('images');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
